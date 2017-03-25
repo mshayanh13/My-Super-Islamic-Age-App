26b500386a88ad6803e33f780ec0026e01bd3abc
mshayanh13 <mohammadhemani@gmail.com>
2017-03-25 01:42:07 -0500

    Initial Commit

---
+++ MySuperIslamicAgeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySuperIslamicAgeApp
+//
+//  Created by Mohammad Hemani on 3/25/17.
+//  Copyright © 2017 Mohammad Hemani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
